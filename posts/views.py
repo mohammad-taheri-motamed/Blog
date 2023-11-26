4c93fa3ba8eb0080ee20dd6700e9d82e2c3046d9
mohammad-taheri-motamed <mohamad.taherii1997@gmail.com>
2023-11-26 22:57:28 +0330

    add swagger and environment vars

--- posts/views.py
+++ posts/views.py
@@ -8,7 +8,7 @@ from rest_framework import viewsets
 from .models import Post
 from .serializers import PostSerializer, UserSerializer
 
-class PostViewSet(viewsets.ModelViewSet): # new
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
